00916fe910ba398e55b87159d7811c1dd2c2b3ee
Markel <markeltskhadadze@gmail.com>
2022-08-02 09:37:18 +0400

    added conditions for displaying messages and checking for an empty string or null

--- script.js
+++ script.js
@@ -8,13 +8,29 @@ let personalMovieDB = {
   privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?');
-let b = +prompt('На сколько оцените его?');
-let c = prompt('Один из последних просмотренных фильмов?');
-let d = +prompt('На сколько оцените его?');
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+  let a = prompt('Один из последних просмотренных фильмов?');
+  let b = +prompt('На сколько оцените его?');
+
+  if (a != null && b != null && a != '' && b != '' && a.length <50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+
+}
 
 console.log(personalMovieDB);
